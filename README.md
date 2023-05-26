505376b1b675a5afaf1d0b2716e3036e3d25e7bc
Peter Bjorklund <piot@hotmail.com>
2023-05-26 11:27:12 +0200

    docs: update readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# atheneum
+# Atheneum
+
+Platform independent way of loading a shared library.
+
+Implemented for Windows (`.dll`), MacOS (`.dylib`) and Linux (`.so`).
+
+## Usage
+
+```c
+int atheneumInit(Atheneum *self, const char* filename); // Loads the specified library from file
+void* atheneumAddress(const Atheneum* self, const char* name); // Returns NULL if not found
+int atheneumClose(Atheneum* self);
+```
--- src/lib/atheneum.c
+++ src/lib/atheneum.c
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 #include <atheneum/atheneum.h>
 
 #if TORNADO_OS_LINUX || TORNADO_OS_MACOS
